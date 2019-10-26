6f1f67cb1b557ea8d2e689fb18b6994a1c579baa
shin52 <b1018088@fun.ac.jp>
2019-10-26 15:23:48 +0900

    First Commit

---
+++ GitHubAPIPractice/GitHubAPIPractice/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GitHubAPIPractice
+//
+//  Created by Itou Shingo on 2019/10/26.
+//  Copyright © 2019 Itou Shingo. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
